0308c56586aedeb8627dbc5113bc26b9bc324002
Valeria Olivares <val.scout.3@gmail.com>
2023-06-07 09:16:15 -0400

    Creando actividad de Faltas y asistencias

---
+++ app/src/main/java/com/example/mensajes/Registrodefaltas.kt
@@ -0,0 +1,11 @@
+package com.example.mensajes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registrodefaltas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrodefaltas)
+    }
+}
