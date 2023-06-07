168699951e9c38a25223f813d5acdbdf7e8d7748
BrandonAllan <allanbrandon34@gmail.com>
2023-06-07 08:35:44 -0400

    Creando layout informacion de tareas

---
+++ app/src/main/java/com/example/mensajes/Informacion_Tarea.kt
@@ -0,0 +1,11 @@
+package com.example.mensajes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Informacion_Tarea : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacion_tarea)
+    }
+}
