da98369fa89cd4888821bd6bbd18a7b3b6f9d90e
pablo-perez-mojix-com <46537688+pablo-perez-mojix-com@users.noreply.github.com>
2023-06-07 08:34:45 -0400

    Creando actividad de ......

---
+++ app/src/main/java/com/example/mensajes/ListaAsistencia.kt
@@ -0,0 +1,11 @@
+package com.example.mensajes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaAsistencia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_asistencia)
+    }
+}
