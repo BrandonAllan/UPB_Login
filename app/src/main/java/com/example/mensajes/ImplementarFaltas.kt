a656e5ac5151eebf88f2021222d272950c4ee883
Valeria Olivares <val.scout.3@gmail.com>
2023-06-07 09:25:27 -0400

    Creando actividad de Faltas y asistencias

--- app/src/main/java/com/example/mensajes/ImplementarFaltas.kt
+++ app/src/main/java/com/example/mensajes/ImplementarFaltas.kt
@@ -3,9 +3,9 @@ package com.example.mensajes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PeligrodeFalta : AppCompatActivity() {
+class ImplementarFaltas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_peligrode_falta)
+        setContentView(R.layout.activity_implementar_faltas)
     }
 }
