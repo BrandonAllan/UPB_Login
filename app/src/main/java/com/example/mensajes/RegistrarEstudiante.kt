551edab6e91a6d1094ddb93b0d7ba397b7133299
AlejandraGMB <alegomu03@gmail.com>
2023-06-07 08:58:35 -0400

    Creando pantalla de asistencia

---
+++ app/src/main/java/com/example/mensajes/RegistrarEstudiante.kt
@@ -0,0 +1,11 @@
+package com.example.mensajes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarEstudiante : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_estudiante)
+    }
+}
