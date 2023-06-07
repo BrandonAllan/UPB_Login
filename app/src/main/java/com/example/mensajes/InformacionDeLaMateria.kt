84fc32e349436c32ede546243aa5d3e32842e7a0
tmph0304e <tommy.pozo95@gmail.com>
2023-06-07 08:23:36 -0400

    Subo la pantalla de información de la materia

---
+++ app/src/main/java/com/example/mensajes/InformacionDeLaMateria.kt
@@ -0,0 +1,11 @@
+package com.example.mensajes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformacionDeLaMateria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacion_de_la_materia)
+    }
+}
